593f4d652444baf5a481b9e9a9515b74b36d925a
kukidevs <kukidevs@gmail.com>
2023-01-08 23:43:17 +0300

    Beta 1.0

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,19 +12,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val buttonLib = findViewById<Button>(R.id.library_button_main)
         buttonLib.setOnClickListener {
-            Toast.makeText(this@MainActivity, "No library provided!", Toast.LENGTH_SHORT).show()
+            val displayIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(displayIntent)
         }
         val buttonSearch =findViewById<Button>(R.id.search_button_main)
         val buttonClickListener: View.OnClickListener = object: View.OnClickListener{
             override fun onClick(p0: View?) {
-                Toast.makeText(this@MainActivity, "It`s no use" +
-                        " searching in nothing!", Toast.LENGTH_SHORT).show()
+                val displayIntent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(displayIntent)
             }
         }
         buttonSearch.setOnClickListener(buttonClickListener)
         val buttonSettings = findViewById<Button>(R.id.settings_button_main)
-        buttonSettings.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Settings page can`t be shown due to the creator`s lack of skills!", Toast.LENGTH_LONG).show()
+        buttonSettings.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
         }
 
     }
